fc3b9ce4717852b9344cf92fcedfdda79b37df6e
Muhammed Ali Khan <mak.official@outlook.com>
2024-01-16 10:33:43 +1100

    basic template

---
+++ src/CleanArchitectureTemplate/Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿namespace Application;
+
+using FluentValidation;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
+
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+        return services;
+    }
+}
